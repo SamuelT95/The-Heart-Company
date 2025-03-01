cdda2d82811218f2a5d8243b642de4d4fcd0feed
Samuel Tjahjadi <stjahjadi@my.bcit.ca>
2025-03-01 11:19:38 -0800

    cms

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,46 @@
-module.exports = {
+import dotenv from "dotenv";
+dotenv.config();
+
+export default {
   siteMetadata: {
-    title: `Gatsby Sydney Ecommerce Theme`,
-    siteUrl: `https://jamm.matter.design`,
+    title: "My E-commerce Site",
   },
-  plugins: [],
+  plugins: [
+    {
+      resolve: "gatsby-source-contentful",
+      options: {
+        spaceId: process.env.CONTENTFUL_SPACE_ID,
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+      },
+    },
+    "gatsby-plugin-image",
+    "gatsby-plugin-netlify-cms",
+  ],
+};
+
+import path from 'path';
+
+export const createPages = async ({ graphql, actions }) => {
+  const { createPage } = actions;
+
+  const result = await graphql(`
+    {
+      allContentfulProduct {
+        nodes {
+          slug
+          id
+        }
+      }
+    }
+  `);
+
+  result.data.allContentfulProduct.nodes.forEach((product) => {
+    createPage({
+      path: `/product/${product.slug}/`,
+      component: path.resolve("./src/templates/product.js"),
+      context: {
+        id: product.id,
+      },
+    });
+  });
 };
